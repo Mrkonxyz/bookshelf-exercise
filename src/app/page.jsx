d0b1e8d76ee452cedd366f0ab0c37f67cf0c65ec
Mrkonxyz <mrkonz@odds.team>
2023-01-29 15:17:37 +0700

    kon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,21 @@
+"use client"
 import AddBookForm from "@/components/AddBookForm";
 import BookList from "@/components/BookList";
-
+import react from "react";
 const HomePage = () => {
-  const books = [{ title: "Clean Code" }];
+  const book = [{ title: "Clean Code" }];
+  const [books, setBooks] = react.useState(book)
+
+  react.useEffect(() => {
+    console.log(books)
+  },[books])
   return (
     <div>
-      <AddBookForm />
+      <AddBookForm addBook={(v) => {
+        setBooks((bookss) => [...bookss, {title:v}])
+
+        
+      }}/>
       <hr />
       <BookList books={books} />
     </div>
--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,12 +1,25 @@
-const AddBookForm = () => {
-  const title = "Domain Driven Design";
+"use client";
+
+import react from "react";
+const AddBookForm = ({ addBook }) => {
+  const [title, setTitle] = react.useState("Domain Driven Design");
+  const onHandlechang = (event) => {
+    setTitle(event.target.value);
+  };
   return (
     <div>
       <h1>ADD TO COLLECTION</h1>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+           addBook(title)
+           setTitle("")
+        }}
+      >
         <div>
           <label>Title </label>
-          <input value={title} />
+          <input onChange={onHandlechang} />
+          <button type="submit">add</button>
           <br />
           {title}
         </div>
